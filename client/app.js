755a980f8274c50a01cdfe0439df5d73aa278588
Maciej Lichoń <maciek.lichon@gmail.com>
2022-06-03 02:35:03 +0200

    Add conversation functionality

--- client/app.js
+++ client/app.js
@@ -1,4 +1,6 @@
 
+const socket = io(); // inicjalizacja klienta (socketa)
+
 const loginForm = document.getElementById("welcome-form");
 const messagesSection = document.getElementById("messages-section");
 const messagesList = document.getElementById("messages-list");
@@ -6,14 +8,13 @@ const addMessageForm = document.getElementById("add-messages-form");
 const userNameInput = document.getElementById("username");
 const messageContentInput = document.getElementById("message-content");
 
-
 let userName = '';
-
 
 const login = e => {
   e.preventDefault();
   if (userNameInput.value) {
     userName = userNameInput.value;
+    socket.emit('join', userName); // emitter nazwy uzytkownika do serwera
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
   } else {
@@ -43,8 +44,11 @@ const addMessage = (author, content) => {
 
 const sendMessage = e => {
   e.preventDefault();
-  if (messageContentInput.value) {
-    addMessage(userName, messageContentInput.value);
+  let messageContent = messageContentInput.value;
+
+  if (messageContent) {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent }); // emmiter eventu message do serwera
     messageContentInput.value = '';
   } else {
     alert('No message found');
@@ -54,3 +58,7 @@ const sendMessage = e => {
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
+
+socket.on('message', (event) => addMessage(event.author, event.content)); // nasluchiwacz na event 'message' od serwera, ktory przy wykryciu wygeneruje html funkcja addMessage
+socket.on('newUser', (user) => addMessage('Chat Bot', `${user} has joined the conversation`));
+socket.on('removeUser', (user) => addMessage('Chat Bot', `${user} has left the conversation`)); 
--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+const io = socket(server); // polaczenie serwera z paczka socket
+
+// tablica zawierajaca wiadomosci, nie ma tu zastosowania z perspektywy uzytkownika, poniewaz archiwalne wiadomosci nie beda przesylane
+// taka tablice mozna potraktowac jako log wiadomosci, do ktorego dostep ma administrator
+const messages = [];
+const users = [];
+
+io.on('connection', (socket) => {
+  // console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => {
+    // console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('join', (userName) => {
+    // console.log('user joins ' + userName);
+    users.push({ name: userName, id: socket.id });
+    socket.broadcast.emit('newUser', userName);
+  })
+  socket.on('disconnect', () => {
+    // console.log('Oh, socket ' + socket.id + ' has left');
+    const disconnectedUser = users.find(user => user.id === socket.id);
+    const userIndex = users.indexOf(disconnectedUser);
+    users.splice(userIndex, 1);
+
+    if (disconnectedUser) {
+      socket.broadcast.emit('removeUser', disconnectedUser.name);
+    };
+  });
+  // console.log('I\'ve added a listener on message and disconnect events \n');
+});
 
 app.use(express.static(path.join(__dirname, '/client')));
 
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
-});
-
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
 });
